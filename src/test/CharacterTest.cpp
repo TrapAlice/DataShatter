a0a3d494cba8b61382ed2d78de0c81e375cb1250
Alex Shelley <alexshelley@brbmoon.com>
2014-01-03 15:30:31 +0900

    Adding additional tests for Character mana and heat

--- src/test/CharacterTest.cpp
+++ src/test/CharacterTest.cpp
@@ -2,24 +2,53 @@
 #include "DataShatter/Character.hpp"
 
 TEST(CharacterHp,
-	Character character;
-	int hp = character.Hp();
-	character.TakeDamage(10);
-	int currentHp = character.Hp();
-	TEST_NEQ(currentHp, hp);
-	TEST_EQ(currentHp, hp - 10);
+	Character c;
+	TEST_EQ(c.Hp(), c.MaxHp());
+	c.TakeDamage(10);
+	TEST_NEQ(c.Hp(), c.MaxHp());
+	TEST_EQ(c.Hp(), c.MaxHp() - 10);
 
-	character.TakeDamage(100000);
-	currentHp = character.Hp();
-	TEST_EQ(currentHp, 0);
+	c.TakeDamage(100000);
+	TEST_EQ(c.Hp(), 0);
 
-	character.RecoverHealth(10);
-	currentHp = character.Hp();
-	TEST_EQ(currentHp, 10);
+	c.RecoverHealth(10);
+	TEST_EQ(c.Hp(), 10);
 
-	character.RecoverHealth(100000);
-	currentHp = character.Hp();
-	int maxHp = character.MaxHp();
-	TEST_EQ(currentHp, maxHp);
+	c.RecoverHealth(100000);
+	TEST_EQ(c.Hp(), c.MaxHp());
+)
+
+TEST(CharacterMana,
+	Character c;
+	TEST_EQ(c.Mana(), c.MaxMana());
+	c.UseMana(10);
+	TEST_NEQ(c.Mana(), c.MaxMana());
+	TEST_EQ(c.Mana(), c.MaxMana() - 10);
+
+	c.UseMana(100000);
+	TEST_EQ(c.Mana(), 0);
+
+	c.GainMana(10);
+	TEST_EQ(c.Mana(), 10);
+
+	c.GainMana(100000);
+	TEST_EQ(c.Mana(), c.MaxMana());
+)
+
+TEST(CharacterHeat,
+	Character c;
+	TEST_EQ(c.Heat(), 0);
+
+	c.GenerateHeat(10);
+	TEST_EQ(c.Heat(), 10);
+
+	c.GenerateHeat(100000);
+	TEST_EQ(c.Heat(), 100);
+
+	c.LoseHeat(10);
+	TEST_EQ(c.Heat(), 90);
+
+	c.LoseHeat(100000);
+	TEST_EQ(c.Heat(), 0);
 )
 
