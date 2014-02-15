32b8ed2a787156ba376af8339a35f5689eef5a95
Alex Shelley <alexshelley@brbmoon.com>
2014-02-15 22:44:42 +0900

    Adding some tests for Conditions

---
+++ src/test/ConditionTest.cpp
@@ -0,0 +1,28 @@
+#include "UnitTest.hpp"
+#include "DataShatter/Condition.hpp"
+#include "DataShatter/GlobalTime.hpp"
+#include "DataShatter/Combatant.hpp"
+
+TEST(Condition, ConditionExpiring){
+	GlobalTime::SetDebugging();
+	GlobalTime::Reset();
+
+	Condition c{ConditionType::On_tick,[](Combatant&, Combatant&, int&){}, 5};
+	TEST_CHECK(!c.isExpired());
+	GlobalTime::SetTime(4);
+	TEST_CHECK(!c.isExpired());
+	GlobalTime::SetTime(5);
+	TEST_CHECK(c.isExpired());
+	GlobalTime::SetTime(6);
+	TEST_CHECK(c.isExpired());
+}
+
+TEST(Condition, ConditionFunction){
+	Condition c{ConditionType::On_defense,
+		[](Combatant&, Combatant&, int& damage){ damage /= 2; }, 5};
+	Combatant blank;
+
+	int damage = 20;
+	c.Activate(blank, blank, damage);
+	TEST_EQ(damage, 10);
+}
