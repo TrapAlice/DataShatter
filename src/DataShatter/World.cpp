84c854b86783c1e69f8b9cc1f7e3b651522060bf
Alex Shelley <alexshelley@brbmoon.com>
2014-02-16 00:39:02 +0900

    fixup! Giving character a sword and shield to start off with

--- src/DataShatter/World.cpp
+++ src/DataShatter/World.cpp
@@ -1,6 +1,10 @@
 #include "World.hpp"
 #include "Character.hpp"
 
+#include "Item.hpp"
+#include "ItemData.hpp"
+#include "Equipment.hpp"
+
 PRIVATE_VARIABLES(World){
 	unique_ptr<Character> character;
 };
@@ -9,6 +13,10 @@ World::World()
 	: INIT_PRIVATE_VARIABLES()
 {
 	m->character = unique_ptr<Character>(new Character());
+	m->character->GiveItem({ItemDataStore::GetData(0)});
+	m->character->GiveItem({ItemDataStore::GetData(1)});
+	m->character->Equip(m->character->Items()[0]);
+	m->character->Equip(m->character->Items()[1], LEFT_HAND);
 }
 
 World::~World(){}
