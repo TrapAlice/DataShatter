213cf8f9fdd81ba6bed1073e1657357069a729b9
Alex Shelley <alexshelley@brbmoon.com>
2014-02-21 22:44:05 +0900

    Equipment is swapped properly again

--- src/DataShatter/Equipment.cpp
+++ src/DataShatter/Equipment.cpp
@@ -3,6 +3,7 @@
 #include "ItemData.hpp"
 #include "Ability.hpp"
 #include "EquipmentSlots.hpp"
+#include "ItemType/ItemTypes.hpp"
 
 #include <array>
 
@@ -19,24 +20,23 @@ Equipment::~Equipment()
 
 void Equipment::Equip(Item const& item)
 {
-	//if( item.Data().EquipSlot == ItemEquipSlot::Hand ){
-	//	if( m->equipment[LEFT_HAND] == &item ){
-	//		m->equipment[LEFT_HAND] = m->equipment[RIGHT_HAND];
-	//	}
+	if( item.Type()->GetEquipSlot() == EquipSlot::Hand ){
+		if( m->equipment[LEFT_HAND] == &item ){
+			m->equipment[LEFT_HAND] = m->equipment[RIGHT_HAND];
+		}
 		m->equipment[RIGHT_HAND] = &item;
-	//}
+	}
 }
 
 void Equipment::Equip(Item const& item, int location)
 {
-	//if( item.Data().EquipSlot == ItemEquipSlot::Hand){
-	//	if( !(location != RIGHT_HAND || location != LEFT_HAND)) return;
-	//	if( location == RIGHT_HAND && m->equipment[LEFT_HAND] == &item){
-	//		m->equipment[LEFT_HAND] = m->equipment[RIGHT_HAND];
-	//	} else if( m->equipment[RIGHT_HAND] == &item ){
-	//		m->equipment[RIGHT_HAND] = m->equipment[LEFT_HAND];
-	//	}
-	//}
+	if( item.Type()->GetEquipSlot() == EquipSlot::Hand ){
+		if( location == RIGHT_HAND && m->equipment[LEFT_HAND] == &item ){
+			m->equipment[LEFT_HAND] = m->equipment[RIGHT_HAND];
+		} else if( m->equipment[RIGHT_HAND] == &item ){
+			m->equipment[RIGHT_HAND] = m->equipment[LEFT_HAND];
+		}
+	}
 	m->equipment[location] = &item;
 }
 
